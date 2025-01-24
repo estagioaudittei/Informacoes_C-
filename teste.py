990574ddd0765568c52b55aa09a398b227ea943a
estagioaudittei <estagio@audittei.com.br>
2025-01-24 17:11:04 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,69 +1,63 @@
 from time import sleep
 
-firstMatrix = [
-    ['.', '.', '.', '.', '11', '.', '22', '.'],
-    ['.', '.', '.', '.', '.', '.', '11', '.'],
-    ['.', '.', '.', '.', '.', '.', '11', '.'],
-    ['.', '.', '.', '.', '.', '.', '11', '.'],
-    ['.', '.', '.', '.', '.', '.', '11', '3'],
-    ['.', '.', '.', '.', '.', '.', '11', '.'],
-    ['.', '.', '.', '.', '.', '.', '11', '.'],
-    ['.', '.', '.', '.', '.', '.', '11', '.'],
+caminhos = dict()
+
+ja_visitados = []
+forca = int(input())
+posicaoInicial = tuple(map(int,input().split(' ')))
+proximos_na_fila = []
+proximos_na_fila.append(posicaoInicial)
+caminhos[posicaoInicial] = None
+matriz = [
+    ['.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','#','.'],
+    ['.','.','.','.','.','.','#','.'],
+    ['.','.','.','.','.','.','#','.'],
+    ['.','.','.','.','.','.','#','.'],
+    ['.','.','.','.','.','.','#','K'],
 ]
-forcaInicial = int(input())
-posicaoInicial = tuple(map(int,input().split(" ")))
+Objetivo = tuple()
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz[0])):
+        if matriz[linha][coluna] == 'K':
+            Objetivo = (linha,coluna)
+            break
 
-def converterVizinhos(posicaoAtual : tuple, matrizBase:list):
-    for i in [-1,0,1]:
-        for j in [-1,0,1]:
 
-            if posicaoAtual[0] + i >= 0 and posicaoAtual[1] + j >= 0 and posicaoAtual[0] + i <= len(matrizBase)-1 and posicaoAtual[1] + j <= len(matrizBase)-1 and j != i:
-                if matrizBase[posicaoAtual[0] + i][posicaoAtual[1] + j] == "A":
-                    pass
-                elif matrizBase[posicaoAtual[0] + i][posicaoAtual[1] + j] == '.' or int(matrizBase[posicaoAtual[0] + i][posicaoAtual[1] + j]) <= forcaInicial :
-                    matrizBase[posicaoAtual[0] + i][posicaoAtual[1] + j] = 'A'
 
-def haVizinhosNaoVerificados(posicaoAtual : tuple, matrizbase: list):
-    for i in [-1,0,1]:
-        for j in [-1,0,1]:
-            if posicaoAtual[0] + i >= 0 and posicaoAtual[1] + j >= 0 and posicaoAtual[0] + i <= len(matrizbase)-1 and posicaoAtual[1] + j <= len(matrizbase)-1 and i != j:
-                if matrizbase[posicaoAtual[0] + i][posicaoAtual[1] + j] == "A":
-                    pass
-                elif matrizbase[posicaoAtual[0] + i][posicaoAtual[1] + j] == '.' or int(matrizbase[posicaoAtual[0] + i][posicaoAtual[1] + j]) <= forcaInicial :
-                    return True
+def adicionarVizinhosNaFila(posicaoAtual : tuple):
+    for proximos_x in [-1,0,1]:
+        for proximos_y in [-1,0,1]:
+            if posicaoAtual[0] + proximos_x >= 0 and posicaoAtual[0] + proximos_x < len(matriz[0]) and posicaoAtual[1] + proximos_y >= 0 and posicaoAtual[1]  + proximos_y < len(matriz):
+                if (posicaoAtual[0] + proximos_x, posicaoAtual[1] + proximos_y) not in ja_visitados and (posicaoAtual[0] + proximos_x, posicaoAtual[1] + proximos_y) not in proximos_na_fila and matriz[posicaoAtual[0] + proximos_x][posicaoAtual[1] + proximos_y] != "#":
+                    proximos_na_fila.append((posicaoAtual[0] + proximos_x, posicaoAtual[1] + proximos_y))
+                    caminhos[(posicaoAtual[0] + proximos_x, posicaoAtual[1] + proximos_y)] = posicaoAtual
 
-    return False
-firstMatrix[posicaoInicial[0]][posicaoInicial[1]] = 'A'
-posicaoAlterada = True
-while True:
-    continuar = False
-    for lista in firstMatrix:
-        if '.' in lista:
-            continuar = True
-    if not posicaoAlterada:
-        print("DEADLOCK")
+
+
+
+for item in proximos_na_fila:
+
+    matriz[item[0]][item[1]] = 'A'
+
+    for linha in matriz:
+        print(linha)
+    adicionarVizinhosNaFila(item)
+    if item == Objetivo:
+        resposta = []
+        while True:
+            if caminhos[item] == None:
+                break
+            else:
+                resposta.append(item)
+                item =  caminhos[item]
+        print("O minimo de passos para chegar até o objetivo é: ", len(resposta))
+
+        # print("Chegou =D")
+        # print(caminhos)
         break
-    if continuar:
-        posicaoAlterada = False
-        for item in range(len(firstMatrix)):
-            for posicao in range(len(firstMatrix[0])):
-                if firstMatrix[item][posicao] == 'A' and haVizinhosNaoVerificados((item,posicao),firstMatrix):
-                    if not posicaoAlterada:
-                        posicaoAtual = (item,posicao)
-                        posicaoAlterada = True
-
-        converterVizinhos(posicaoAtual,firstMatrix)
-
-
-        for linhaMatriz in firstMatrix:
-            print(linhaMatriz)
-        # sleep(2)
-    else:
-        break
-
-
-
-
 
 
 
